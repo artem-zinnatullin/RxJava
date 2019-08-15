55e721e06385856dc5b8089372e4b4085796239c
Artem Zinnatullin <ceo@artemzin.com>
2019-08-15 14:25:03 -0700

    Add basic 3.x Observable interop

--- src/main/java/io/reactivex/Observable.java
+++ src/main/java/io/reactivex/Observable.java
@@ -15487,4 +15487,40 @@ public abstract class Observable<T> implements ObservableSource<T> {
         subscribe(to);
         return to;
     }
+
+    public final io.reactivex.rxjava3.Observable<T> toV3Observable() {
+        // TODO proper impl.
+        final Observable<T> v2Observable = this;
+        return io.reactivex.rxjava3.Observable.create(new io.reactivex.rxjava3.ObservableOnSubscribe<T>() {
+            @Override
+            public void subscribe(final io.reactivex.rxjava3.ObservableEmitter<T> emitter) throws Throwable {
+                v2Observable.subscribeWith(new Observer<T>() {
+                    @Override
+                    public void onSubscribe(final Disposable d) {
+                        emitter.setDisposable(io.reactivex.rxjava3.disposables.Disposables.fromRunnable(new Runnable(){
+                            @Override
+                            public void run() {
+                                d.dispose(); // TODO port disposable properly.
+                            }
+                        }));
+                    }
+
+                    @Override
+                    public void onNext(T t) {
+                        emitter.onNext(t);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        emitter.onError(e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        emitter.onComplete();
+                    }
+                });
+            }
+        });
+    }
 }
